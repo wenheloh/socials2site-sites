4f5bebdac4910ff5235afcfdc8bd18caefec5ca5
Loh Wen He <wenheloh@hotmail.com>
2020-09-08 09:58:09 +0800

    [WH] Fix CSS issue on header nav menu

--- components/structural/TopBar/NavigationDropdown.vue
+++ components/structural/TopBar/NavigationDropdown.vue
@@ -1,15 +1,12 @@
 <template>
-  <ul class="nav-menus">
-    <nuxt-link
-      v-for="(subMenu, index) of subMenus"
-      :key="index"
-      class="nuxt-link"
-      :class="{ 'nav-menu-selected': currentPath === subMenu.route }"
-      :to="subMenu.route"
-      :title="subMenu.name"
-    >
-      <li>{{ subMenu.name }}</li>
-    </nuxt-link>
+  <ul class="ulNavMenus">
+    <li v-for="(subMenu, index) of subMenus" :key="index" @click="setCurrentPath">
+      <nuxt-link
+        :class="{ 'ulNavMenusSelected': currentPath === subMenu.route }"
+        :to="subMenu.route"
+        :title="subMenu.name"
+      >{{ subMenu.name }}</nuxt-link>
+    </li>
   </ul>
 </template>
 
@@ -17,24 +14,34 @@
 import { Context } from "@nuxt/types";
 import { Component, Vue } from "vue-property-decorator";
 
+interface MenuAction {
+  name: string;
+  route: string;
+}
+
+@Component
 export default class NavigationDropdown extends Vue {
   // TODO: Call api to get submenus?
-  private subMenus = [
+  private subMenus: MenuAction[] = [
     {
       name: "Home",
-      route: "/"
+      route: "/",
     },
     {
       name: "About Us",
-      route: "/about-us"
+      route: "/about-us",
     },
     {
       name: "Contact Us",
-      route: "/contact-us"
-    }
+      route: "/contact-us",
+    },
   ];
 
-  private currentPath = this.$nuxt.$route.path;
+  private currentPath: string = this.$nuxt.$route.path;
+
+  private setCurrentPath() {
+    this.currentPath = this.$nuxt.$route.path;
+  }
 }
 </script>
 
