d12f649bd3d673d5c4ff762f963022dd8fe0253c
Loh Wen He <wenheloh@hotmail.com>
2020-09-20 22:31:51 +0800

    [WH] Added `ResponsiveGrid` to be used in subpages for Facebook post kind of contents

---
+++ components/structural/ResponsiveGrid/ResponsiveGridContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="divResponsiveGridContainer" />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class ResponsiveGridContainer extends Vue {
+  // TODO: Get posts data via API?
+}
+</script>
