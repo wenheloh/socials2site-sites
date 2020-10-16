4705cf7d508bbfb2429a4623cef030c1245f1e82
Loh Wen He <wenheloh@hotmail.com>
2020-10-17 01:32:12 +0800

    [STS-22][WH] testing dynamic route

---
+++ pages/_.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="height: 100%">
+    Halo I'm dynamic ok 🙂
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Dynamic extends Vue {
+}
+</script>
